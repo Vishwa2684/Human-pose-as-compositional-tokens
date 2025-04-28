41ef7988891c76c4b83128096c43cefe4936bb36
Vishwa <vishwateja2684@gmail.com>
2025-04-28 21:34:41 +0530

    first commit

---
+++ model/encoder.py
@@ -0,0 +1,50 @@
+import torch.nn as nn
+from model.modules import FCBlock, MixerLayer
+
+class CompositionalEncoder(nn.Module):
+    def __init__(self,k,d,h,m,dropout_ratio=0.0):
+        super().__init__()
+        self.linear1 = nn.Linear(d,h)
+        self.mixer = MixerLayer(hidden_dim=h,hidden_inter_dim=h,token_inter_dim=k,token_dim=k,dropout_ratio=dropout_ratio)
+        self.linear2 = nn.Linear(k,m)
+    def forward(self,x):
+        x = self.linear1(x)
+        x = self.mixer(x)
+        x = x.transpose(1,2)
+        x = self.linear2(x)
+        x = x.transpose(1,2)
+        return x
+
+# utilizes EMA
+class VectorQuantizer(nn.Module):
+    def __init__(self, v, h, commitment_cost):
+        super().__init__()
+        self.num_codes = v
+        self.code_dim = h
+        self.commitment_cost = commitment_cost
+        # randomly initialized codebook (v x h) which can be learned
+        self.codebook = nn.Parameter(torch.randn(v, h))
+
+    def forward(self, x):
+        # x: (batch_size, M tokens, code_dim)
+        b, m, h = x.shape
+        flat_x = x.view(-1, h)  # (b*m, h)
+
+        # compute distances
+        distances = (
+            flat_x.pow(2).sum(1, keepdim=True)
+            - 2 * flat_x @ self.codebook.t()
+            + self.codebook.pow(2).sum(1)
+        )  # (b*m, v)
+
+        encoding_indices = torch.argmin(distances, dim=1)  # (b*m)
+
+        quantized = self.codebook[encoding_indices]  # (b*m, h)
+
+        # compute loss
+        commitment_loss = F.mse_loss(quantized.detach(), flat_x)
+        codebook_loss = F.mse_loss(quantized, flat_x.detach())
+        loss = self.commitment_cost * commitment_loss + codebook_loss
+
+        return quantized.view(b, m, h), loss, encoding_indices
+ 
---
+++ process/data.py
@@ -0,0 +1,42 @@
+from torch.utils.data import Dataset
+import os
+import cv2
+import torch
+import pandas as pd
+import torchvision.transforms as transforms
+import torch.nn as nn
+
+class MPIIDataset(Dataset):
+    def __init__(self, csv_path, image_dir, transform=None):
+        self.kp = pd.read_csv(csv_path)
+        self.image_dir = image_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.kp)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.image_dir, self.kp['NAME'][idx])
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        h, w, _ = image.shape
+
+        keypoints = []
+        for joint in [
+            'r ankle', 'r knee', 'r hip',
+            'l ankle', 'l knee', 'l hip',
+            'pelvis', 'thorax', 'upper neck', 'head top',
+            'r wrist', 'r elbow', 'r shoulder',
+            'l wrist', 'l elbow', 'l shoulder']:
+            x, y = self.kp[f'{joint}_X'][idx], self.kp[f'{joint}_Y'][idx]
+            x, y = float(x), float(y)
+            if x == -1 or y == -1:
+                x, y = 0.0, 0.0
+            keypoints.extend([x / w, y / h])
+
+        keypoints = torch.tensor(keypoints, dtype=torch.float32)
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, keypoints
