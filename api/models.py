b44eab5aad1b6fac82f78e3602e3c50a2ebec6db
Vishwa <vishwateja2684@gmail.com>
2025-05-09 15:49:25 +0530

    wrote code to pull stage 1 weights

---
+++ api/models.py
@@ -0,0 +1,19 @@
+import gdown
+import os
+
+# Replace this with your public Google Drive shareable link
+
+def load_decoder_and_quantizer_weights():
+    if(os.path.exists("./weights/50.pt")):
+        print("Weights already downloaded")
+        return
+    # Extract the file ID from the shareable link (e.g., "FILE_ID" in the link
+    url = "https://drive.google.com/file/d/1an2hhD202GZdOmz7cRYSsc4LDJFNMvMK/view?usp=sharing"
+    output_dir = "./weights"
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, "50.pt")  # Replace with desired filename
+
+    # Download the file
+    gdown.download(url, output_path, quiet=False)
+
+load_decoder_and_quantizer_weights()
