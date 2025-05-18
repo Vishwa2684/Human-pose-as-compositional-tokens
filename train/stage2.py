8df93a97da0e164f58d63ce044b5a17487acb426
Vishwa <vishwateja2684@gmail.com>
2025-05-18 17:28:53 +0530

    loading backbone of swinv2 in1k from timm

--- train/stage2.py
+++ train/stage2.py
@@ -1,15 +1,23 @@
 import os
 import torch
+from dotenv import load_dotenv
 from model.modules import BasicBlock,MixerLayer
 from model.encoder import CompositionalEncoder
 from process.data import MPIIDataset
 from api.models import load_decoder_and_quantizer_weights
+import warnings
+from huggingface_hub import login
+import timm
+warnings.filterwarnings("ignore")
+load_dotenv()
 
-
+login(token=os.getenv('HUGGINGFACE_TOKEN'))
+# print(*(m+"\n" for m in timm.list_models() if 'swinv2' in m))
 
 # CONFIG
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 NUM_JOINTS = 16
+IMAGE_SIZE = 256
 DIMENSION = 2
 HIDDEN_DIM = 256
 m = 16
@@ -21,7 +29,11 @@ encoder_weights = torch.load(os.path.join(os.getcwd(),'weights','50.pt'),map_loc
 # Loading Encoder
 encoder = CompositionalEncoder(k=NUM_JOINTS,d=DIMENSION,h=HIDDEN_DIM,m=m).to(device)
 encoder.load_state_dict(encoder_weights['encoder'])
+print('encoder weights loaded')
 
+backbone = timm.create_model('swinv2_base_window12to16_192to256.ms_in22k_ft_in1k', pretrained=True)
+backbone.to(device)
+print('backbone loaded')
 # backbone extracts image features X
 
 # then use 2 basic residual convolutional blocks to modulate features of X.
