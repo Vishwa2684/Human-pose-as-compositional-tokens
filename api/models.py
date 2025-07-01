3dc7c20bce05a90e8a8f65a973fc352b625b6b4d
Vishwa2684 <vishwarocks1234@gmail.com>
2025-07-01 04:32:40 +0000

    change in download link

--- api/models.py
+++ api/models.py
@@ -8,7 +8,7 @@ def load_decoder_and_quantizer_weights():
         print("Weights already downloaded")
         return
     # Extract the file ID from the shareable link (e.g., "FILE_ID" in the link
-    url = "https://drive.google.com/uc?export=download&id=1an2hhD202GZdOmz7cRYSsc4LDJFNMvMK"
+    url = "https://drive.google.com/uc?export=download&id=1IwBluuvQYV7pU3aFJ_Y2lJ9pyUkJoI0x"
     output_dir = "./weights"
     os.makedirs(output_dir, exist_ok=True)
     output_path = os.path.join(output_dir, "50.pt")  # Replace with desired filename
